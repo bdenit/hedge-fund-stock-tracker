ac57317a850deb26c1d8bcf9b648a4c3e2ea3aeb
bdenit <brad.dening@gmail.com>
2026-05-05 15:08:57 +0930

    Update app.py

--- app.py
+++ app.py
@@ -3,37 +3,14 @@ import yfinance as yf
 import pandas as pd
 import json
 import os
-import setuptools
 from datetime import datetime, timedelta
 
 try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
+    import pyasx.data.companies
+
+    PYASX_AVAILABLE = True
 except ImportError:
-    long_description = open("README.md").read()
-    print("NOTE documentation not generated correctly, requires `pypandoc`")
-
-
-setuptools.setup(
-    name='pyasx',
-    version='2.1.7',
-    description='Python library to pull data from ASX.com.au',
-    long_description=long_description,
-    url='http://github.com/zacscott/pyasx',
-    author='Zac Scott',
-    author_email='zac@zacscott.net',
-    license='MIT',
-    packages=setuptools.find_packages(
-        exclude=['tests',]
-    ),
-    package_data={'pyasx': ['*.yml']},
-    python_requires='>=2.6',
-    install_requires=[
-        'requests',
-        'pyyaml',
-        'python-dateutil'
-    ]
-)
+    PYASX_AVAILABLE = False
 
 # VADER Sentiment
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
