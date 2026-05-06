9031ca30cc80205fa5454a686de3dddcd4827ebe
bdenit <brad.dening@gmail.com>
2026-05-06 12:17:07 +0930

    Update app.py

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ with tab1:
                 "Current Price": pnl["current_price"],
                 "Market Value": mv,
                 "Daily %": pnl["daily_change"],
-                "Dividend Yield %": dividend_yield if dividend_yield is not None else "N/A",
+                "Dividend Yield": dividend_yield if dividend_yield is not None else "N/A",
                 "Unrealized P&L": pnl["unrealized_pnl"],
                 "Sector": sector,
                 "Industry": industry
