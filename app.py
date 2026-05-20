4595dc65ed2b229b7e901dd8b687fa7b40f3e88a
bdenit <brad.dening@gmail.com>
2026-05-20 15:17:31 +0930

    Update app.py

--- app.py
+++ app.py
@@ -247,7 +247,6 @@ print(f"Dividend Yield: {info.get('dividendYield')}")
 # 2. Get history of dividend payments
 # This returns a pandas Series with dates and amounts
 dividends = ticker.dividends
-latest_dividend = all_dividends.tail(1)
 print(dividends.tail()) # Shows recent payouts
 
 with tab4:
